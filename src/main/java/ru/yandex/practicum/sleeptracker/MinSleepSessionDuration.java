ca008e14f29ff0ad7cd75cf7775a666c61e6c1d3
Aleksandr Nizamov <alexandr.nizamov@mail.ru>
2026-02-24 01:38:18 +0500

    Исправление CheckStyle

--- src/main/java/ru/yandex/practicum/sleeptracker/MinSleepSessionDuration.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/MinSleepSessionDuration.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.sleeptracker;
 
 import java.time.Duration;
-import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
